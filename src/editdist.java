fae34b2fe674e69c7d42688de4a21a3daaaa48de
Karthik R Kasarla <kkasarla@musiusa.com>
2018-06-25 10:24:55 -0400

    Revert "Revert "print statement changed""
    This reverts commit 011c102761d28e082be2fb47abfa14899f226aa6.

--- src/editdist.java
+++ src/editdist.java
@@ -15,7 +15,7 @@ public class editdist {
 		/*for(int i=0;i<in.length;i++){
 			System.out.println(in[i]);
 		}*/
-		System.out.println("enter the transformed string");
+		System.out.println("enter the string transformed");
 		String output=scann.nextLine();
 		char[] out=output.toCharArray();
 		int inlen=in.length;
